57aee91901d7f0f7cf3e94d936f4087cd07e8033
AndrewKchn <andrewkchn@gmail.com>
2020-02-25 11:13:23 +0200

    Initial project

---
+++ src/main/java/com/softserve/task6_happy_tickets/Ticket.java
@@ -0,0 +1,18 @@
+package com.softserve.task6_happy_tickets;
+
+public class Ticket {
+    private int number;
+
+    Ticket(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket№ " + String.format("%06d",number);
+    }
+}
